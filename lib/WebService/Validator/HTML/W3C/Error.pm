d8c6cd0677da1c9195b10a24d2f0083462be6dac
Struan Donald <struan@exo.org.uk>
2003-11-24 21:55:59 +0000

    some rudimentary docs added
    git-svn-id: file:///usr/local/svn-public/misc/webservice-validator-html-w3c/trunk@23 a4aac765-74e8-0310-878a-868ca5cf1531

--- lib/WebService/Validator/HTML/W3C/Error.pm
+++ lib/WebService/Validator/HTML/W3C/Error.pm
@@ -1,4 +1,4 @@
-# $Id: Error.pm,v 1.2 2003/11/24 21:50:32 struan Exp $
+# $Id: Error.pm,v 1.3 2003/11/24 21:55:59 struan Exp $
 
 package WebService::Validator::HTML::W3C::Error;
 
@@ -9,3 +9,18 @@ __PACKAGE__->mk_accessors( qw( line col msg ) );
 
 1;
 
+=head1 NAME WebService::Validator::HTML::W3C::Error
+
+=head1 DESCRIPTION
+
+This is a wee internal module for WebService::Validator::HTML::W3C. It has
+three methods: line, col and msg which return the line number, column number 
+and the error that occured at that location in a validated page.
+
+See the WebService::Validator::HTML::W3C Documentation for more details.
+
+=head1 AUTHOR
+
+Struan Donald
+
+=cut
